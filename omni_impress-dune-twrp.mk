dcf0707079388c63846b58d5c209595b8ba06bba
zedd-02ru <34280337+zedd-02ru@users.noreply.github.com>
2017-12-05 23:45:51 +0500

    Update and rename omni_impress-dune.mk to omni_impress-dune-twrp.mk

--- omni_impress-dune-twrp.mk
+++ omni_impress-dune-twrp.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 PRODUCT_COPY_FILES := \
-    device/vertex/impress-dune/prebuilt/kernel:kernel \
+    device/vertex/impress-dune-twrp/prebuilt/kernel:kernel \
     bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
 PRODUCT_RELEASE_NAME := Vertex
-PRODUCT_NAME := omni_impress-dune
-PRODUCT_DEVICE := dune
+PRODUCT_NAME := omni_impress-dune-twrp
+PRODUCT_DEVICE := impress-dune
 PRODUCT_BRAND := Vertex
 PRODUCT_MODEL := Vertex Impress Dune
 PRODUCT_MANUFACTURER := Vertex
