4990f77a852aaa5f418b2488be1832f433786bde
zedd-02ru <34280337+zedd-02ru@users.noreply.github.com>
2018-03-26 13:30:09 +0500

    Update and rename omni_duna.mk to omni_impress.mk

--- omni_impress.mk
+++ omni_impress.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 PRODUCT_COPY_FILES := \
-    device/vertex/duna/prebuilt/kernel:kernel \
+    device/vertex/impress/prebuilt/kernel:kernel \
     bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
 PRODUCT_RELEASE_NAME := Vertex
-PRODUCT_NAME := omni_duna
-PRODUCT_DEVICE := duna
+PRODUCT_NAME := omni_impress
+PRODUCT_DEVICE := Dune
 PRODUCT_BRAND := Vertex
-PRODUCT_MODEL := Vertex Impress Duna
+PRODUCT_MODEL := Vertex Impress Dune
 PRODUCT_MANUFACTURER := Vertex
